f7e26bb6048b26d89ed6e54628c1875e5d42b755
stephenjtyrrell <stephenjtyrrell@users.noreply.github.com>
2024-10-15 19:42:58 +0100

    Adding Person logic Adding basic logic to the main class to have something there for unit testing

--- ConsoleAppForUnitTesting/Program.cs
+++ ConsoleAppForUnitTesting/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Person
+{
+  private string name; // field
+  public string Name   // property
+  {
+    get { return name; }
+    set { name = value; }
+  }
+}
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+
+    Person myObj = new Person();
+    foreach(string name in args){
+        myObj.Name = name;
+    }
+    Console.WriteLine(myObj.Name);
+  }
+}
